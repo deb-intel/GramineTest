77842ca14df6ff37e530c069b164fec13f133306
Carteepaul <paulx.alan.cartee@intel.com>
2023-02-14 13:02:00 -0700

    reversing some TOC formatting changes.

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -162,7 +162,7 @@ html_theme_options = {
     'logo_only': True,
     # Toc options
     'collapse_navigation': True,
-    'sticky_navigation': True,
+    'sticky_navigation': False,
     'navigation_depth': 2,
     'includehidden': True,
     'titles_only': False    
